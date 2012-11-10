fdcac71eb927531f02e0332a908653392789f53c
Ralph Bodenner <ralph@newrelic.com>
2012-11-09 22:55:45 -0800

    Revert "Try adding STATICFILES_DIRS"
    This reverts commit c6f76e2b7d69a43778481afa01fb57a789810268.

--- settings.py
+++ settings.py
@@ -218,13 +218,6 @@ STATIC_URL = "/static/"
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    STATIC_ROOT,
-)
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
