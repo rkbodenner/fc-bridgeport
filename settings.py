4d318471b7a2ed9b29cbbd4ecd02d35c8f09f464
Ralph Bodenner <ralph@newrelic.com>
2012-11-10 00:08:11 -0800

    BridgePort logo and new default tagline

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 ######################
 
 SITE_TITLE = "FC Bridgeport"
-SITE_TAGLINE = "Fitba and beer, aye"
+SITE_TAGLINE = "A football club in Portland, OR"
 
 AUTH_PROFILE_MODULE = "players.Player"
 
@@ -217,6 +217,10 @@ STATIC_URL = "/static/"
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, "staticfiles"),
+)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
